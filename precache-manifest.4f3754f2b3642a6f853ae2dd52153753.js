85475bf3cdf257878e3fef18bc1b5d28c1aa78ce
Khang Le Thanh <khang.lethanh@student.tut.fi>
2020-08-29 16:00:45 +0300

    Updates

---
+++ precache-manifest.4f3754f2b3642a6f853ae2dd52153753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c4f6d22c88e69bb27a7fb67d58d2de",
+    "url": "/KhangLeThanh/jsoneditor/index.html"
+  },
+  {
+    "revision": "ce9fc6f2afa491a4da6e",
+    "url": "/KhangLeThanh/jsoneditor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e1598ba8d72a89db9e10",
+    "url": "/KhangLeThanh/jsoneditor/static/js/2.e6d32d95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KhangLeThanh/jsoneditor/static/js/2.e6d32d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9fc6f2afa491a4da6e",
+    "url": "/KhangLeThanh/jsoneditor/static/js/main.2212d209.chunk.js"
+  },
+  {
+    "revision": "1c2528bdd575e3deb847",
+    "url": "/KhangLeThanh/jsoneditor/static/js/runtime-main.718cdf6c.js"
+  }
+]);
